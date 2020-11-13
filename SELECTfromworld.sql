cdbc9fa6637657a365669f8479c27e268d7d8798
husinassegaff <63222585+husinassegaff@users.noreply.github.com>
2020-11-13 16:58:11 +0700

    Completed for section select from world

--- SELECTfromworld.sql
+++ SELECTfromworld.sql
@@ -42,3 +42,48 @@ SELECT name FROM world
 
 SELECT name, population, area FROM world
     WHERE area >= 3000000 OR population >= 250000000
+
+
+-- 8. Show the countries that are big by area (more than 3 million) 
+--    or big by population (more than 250 million) but not both. 
+--    Show name, population and area.
+
+SELECT name, population, area FROM world
+    WHERE(area > 3000000 AND population < 250000000) OR
+    (area < 3000000 AND population > 250000000)
+
+
+-- 9. For South America show population in millions and GDP in billions 
+--    both to 2 decimal places.
+
+SELECT name, ROUND(population/1000000,2) , ROUND(GDP/1000000000,2) FROM world
+    WHERE continent LIKE 'South America'
+
+
+-- 10. Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+
+SELECT name, ROUND(GDP/population,-3) FROM world
+    WHERE GDP >= 1000000000000
+
+
+-- 11. Show the name and capital where the name and the capital have the same number 
+--     of characters.
+
+SELECT name, capital FROM world
+    WHERE LENGTH(name) = LENGTH(capital)
+
+
+-- 12. Show the name and the capital where the first letters of each match. 
+--     Don't include countries where the name and the capital are the same word.
+
+SELECT name, capital FROM world
+    WHERE LEFT(name,1) = LEFT(capital,1) AND name <> capital
+
+
+-- 13. Find the country that has all the vowels and no spaces in its name.
+
+SELECT name FROM world
+    WHERE (name NOT LIKE '%a%a%') AND (name NOT LIKE '%i%i%')
+    AND (name NOT LIKE '%u%u%') AND (name NOT LIKE '%e%e%') AND
+    (name NOT LIKE '%o%o%') AND (name LIKE '%a%') AND (name LIKE '%i%')
+    AND (name LIKE '%u%') AND (name LIKE '%e%') AND (name LIKE '%o%')
