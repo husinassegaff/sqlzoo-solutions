074d4cfea03803584497810b647bbc672b959611
husinassegaff <63222585+husinassegaff@users.noreply.github.com>
2020-11-25 22:29:08 +0700

    Completed for section select with select

--- SELECTwithinselect.sql
+++ SELECTwithinselect.sql
@@ -66,3 +66,20 @@ SELECT continent, name FROM world x
     WHERE x.name = (SELECT y.name FROM world y
         WHERE y.continent = x.continent
          ORDER BY name LIMIT 1)
+
+
+-- 9. Find the continents where all countries have a population 
+--    <= 25000000. Then find the names of the countries associated 
+--    with these continents. Show name, continent and population.
+
+SELECT name, continent, population FROM world x
+    WHERE 25000000 >= ALL(SELECT population FROM world y
+     WHERE (x.continent = y.continent) AND (population > 0))
+
+--  10. Some countries have populations more than three times that 
+--      of any of their neighbours (in the same continent). Give the 
+--      countries and continents.
+
+SELECT name, continent FROM world x
+    WHERE x.population/3 >= ALL(SELECT population FROM world y
+     WHERE (x.continent = y.continent) AND (x.name != y.name) AND (y.population > 0))
