d7191b312a30a02d298f55dbd623bc8fe3ed24f7
husinassegaff <63222585+husinassegaff@users.noreply.github.com>
2021-01-03 22:54:37 +0700

    Added solutions for 3-8

--- moreJOINoperations.sql
+++ moreJOINoperations.sql
@@ -1,10 +1,50 @@
---  1. List the films where the yr is 1962 [Show id, title]
+-- 1. List the films where the yr is 1962 [Show id, title]
 
 SELECT id, title FROM movie
     WHERE yr = 1962
 
 
---  2. Give year of 'Citizen Kane'.
+-- 2. Give year of 'Citizen Kane'.
 
 SELECT yr FROM movie
     WHERE title = 'Citizen Kane'
+
+
+-- 3. List all of the Star Trek movies, include the id, title and yr 
+--    (all of these movies include the words Star Trek in the title). Order results by year.
+
+SELECT id, title, yr FROM movie
+    WHERE title LIKE 'Star Trek%'
+        ORDER BY yr
+
+
+-- 4. What id number does the actor 'Glenn Close' have?
+
+SELECT id FROM actor
+    WHERE name = 'Glenn Close'
+
+
+-- 5. What is the id of the film 'Casablanca'
+
+SELECT id FROM movie
+    WHERE title = 'Casablanca'
+
+
+-- 6. Obtain the cast list for 'Casablanca'. Use movieid=11768, (or whatever value you got from the previous question)
+
+SELECT actor.name FROM actor
+    JOIN casting ON ( casting.actorid = actor.id && casting.movieid = 11768)
+
+
+-- 7. Obtain the cast list for the film 'Alien'
+
+SELECT actor.name FROM actor
+    JOIN casting ON casting.actorid = actor.id 
+        JOIN movie ON (movie.id = casting.movieid && movie.title = 'Alien')
+
+
+-- 8. List the films in which 'Harrison Ford' has appeared
+
+SELECT movie.title FROM movie 
+    JOIN casting ON casting.movieid = movie.id 
+        JOIN actor ON (actor.id = casting.actorid && actor.name = 'Harrison Ford') 
